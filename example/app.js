739001bf2142ea30c9e2f65180884653358e02a8
James Homer <jameshomer85@gmail.com>
2014-09-10 11:36:39 +0100

    update example

--- example/app.js
+++ example/app.js
@@ -1,13 +1,19 @@
 angular.module('ExampleCtrl', []).controller('ExampleCtrl', ['$scope',
     function($scope) {
 
-        $scope.items = [];
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
 
         $scope.generateItems = function() {
-            for (var i = 0; i < 50; i++) {
+            $scope.items = [];
+
+            var itemCount = getRandomInt(4, 50);
+
+            for (var i = 0; i < itemCount; i++) {
 
                 $scope.items[i] = {
-                    ratio: Math.random() * 2,
+                    ratio: Math.max(0.5, Math.random() * 2),
                     color: '#' + ('000000' + Math.floor(Math.random() * 16777215).toString(16)).slice(-6)
                 };
 
